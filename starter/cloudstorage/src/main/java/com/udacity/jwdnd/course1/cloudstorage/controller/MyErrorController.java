f8695389dd126c08eb92fb1aeb17cf01738d1d55
ebruogdur <ebru_ogdur@hotmail.com>
2020-09-15 10:31:17 +0300

    added error page

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/MyErrorController.java
@@ -0,0 +1,38 @@
+package com.udacity.jwdnd.course1.cloudstorage.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class MyErrorController implements ErrorController {
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        // get error status
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            // display specific error page
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "404";
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "500";
+            } else if (statusCode == HttpStatus.FORBIDDEN.value()) {
+                return "403";
+            }
+        }
+
+        return "error";
+    }
+}
